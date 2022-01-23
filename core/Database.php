43eb080d1e1bd0108281f3f3ff020724db1f2279
BakhadyrovF <BakhadyrovF@gmail.com>
2022-01-23 13:55:20 +0500

    add function createTable

--- core/Database.php
+++ core/Database.php
@@ -11,6 +11,15 @@ class Database
     public function __construct()
     {
         $this->pdo = new \PDO("mysql:host=localhost;port=3306;dbname=mvc_01", "root", "");
+        $this->createTable();
+    }
+
+    public function createTable()
+    {
+        $statement = $this->pdo->prepare("CREATE TABLE IF NOT EXISTS users 
+        (id INT PRIMARY KEY AUTO_INCREMENT, firstname VARCHAR(255) NOT NULL,
+        email VARCHAR(255) UNIQUE NOT NULL, lastname VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL)");
+        $statement->execute();
     }
 
     
